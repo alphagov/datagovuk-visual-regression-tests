c8c9b253dbfb878568fae25b432b7020e0bcc69a
owenatgov <64783893+owenatgov@users.noreply.github.com>
2020-05-28 11:56:25 +0100

    Minor wording update for form errors console.log

--- backstop_data/engine_scripts/puppeteer/utils/form-errors.js
+++ backstop_data/engine_scripts/puppeteer/utils/form-errors.js
@@ -3,7 +3,7 @@ const login = require('./login');
 module.exports = async (page, scenario) => {
     await login(page, scenario);
 
-    console.log(`Submitting form for scenario ${scenario.label}...`);
+    console.log(`Submitting incomplete form for scenario ${scenario.label}...`);
     await Promise.all([
         page.click('button[type="submit"]'),
         page.waitForNavigation()
