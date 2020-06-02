2a6382bb55d1d4ca63ad02ef88537fb68210e360
owenatgov <64783893+owenatgov@users.noreply.github.com>
2020-06-02 11:41:35 +0100

    Add error catching for puppeteer
    This stops puppeteer from hanging after an error and preventing the cli from closing because the browser session hasn't been closed.

--- setup/index.js
+++ setup/index.js
@@ -5,20 +5,24 @@ const publisher = require('./publisher');
 
 (async () => {
     console.log('Welcome to local data setup for datagovuk visual regression tests. Please ensure that you are running a local instance of docker-ckan and that your environment is clear. Failure to meet these requirements may lead to this script failing and your tests to fail unecessarily.');
-    console.log('Preparing puppeteer...')
+    console.log('Preparing puppeteer...');
+
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    if (process.argv.length < 3) {
-        console.log('Error: You need to pass either "all" or a relevant keyword to this setup script so that it knows which setup scripts to run. Ending script...');
+    try {
+        if (process.argv.length < 3) {
+            throw new Error('Error: You need to pass either "all" or a relevant keyword to this setup script so that it knows which setup scripts to run. Ending script...');
+        }
+
+        await login(page, {url: `${process.env.DOMAIN}`}, 'data setup');
+
+        if (process.argv.indexOf('publisher') || process.argv.indexOf('all')) {
+            await publisher(page);
+        }
+    } catch(e) {
         await browser.close();
-        return;
-    }
-
-    await login(page, {url: `${process.env.DOMAIN}`}, 'data setup');
-
-    if (process.argv.indexOf('publisher') || process.argv.indexOf('all')) {
-        await publisher(page);
+        throw new Error(e);
     }
 
     await browser.close();
