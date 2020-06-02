a6e1c20e28897145e25b9dfdf5b80af2528b7651
owenatgov <64783893+owenatgov@users.noreply.github.com>
2020-06-02 17:25:50 +0100

    Add test harvest setup

--- setup/index.js
+++ setup/index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const puppeteer = require('puppeteer');
 const login = require('../backstop_data/engine_scripts/puppeteer/utils/login');
 const publisher = require('./publisher');
+const harvest = require('./harvest');
 
 (async () => {
-    console.log('Welcome to local data setup for datagovuk visual regression tests. Please ensure that you are running a local instance of docker-ckan and that your environment is clear. Failure to meet these requirements may lead to this script failing and your tests to fail unecessarily.');
+    console.log('Welcome to local data setup for datagovuk visual regression tests. Please ensure that you are running a local instance of docker-ckan and that your environment is clear. Failure to meet these requirements may lead to this script failing and your backstop tests to fail unecessarily.');
+    
     console.log('Preparing puppeteer...');
 
     const browser = await puppeteer.launch();
@@ -12,13 +14,17 @@ const publisher = require('./publisher');
 
     try {
         if (process.argv.length < 3) {
-            throw new Error('Error: You need to pass either "all" or a relevant keyword to this setup script so that it knows which setup scripts to run. Ending script...');
+            throw new Error('Error: You need to pass either "all" or a relevant keyword to this setup script so that it knows which setup scripts to run.');
         }
 
         await login(page, {url: `${process.env.DOMAIN}`}, 'data setup');
 
-        if (process.argv.indexOf('publisher') || process.argv.indexOf('all')) {
+        if (process.argv.indexOf('publisher') !== -1 || process.argv.indexOf('all') !== -1) {
             await publisher(page);
+        }
+
+        if (process.argv.indexOf('harvest') !== -1 || process.argv.indexOf('all') !== -1) {
+            await harvest(page);
         }
     } catch(e) {
         await browser.close();
--- setup/route-status.js
+++ setup/route-status.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
 
-exports.publisher = async page => {
-    const response = await page.goto(`${process.env.DOMAIN}/publisher/test-publisher`);
+const getStatus = async (page, route) => {
+    const response = await page.goto(`${process.env.DOMAIN}/${route}`);
     return response.status();
-};
+}
 
-exports.harvest = async page => {
-    const response = await page.goto(`${process.env.DOMAIN}/harvest/mock-harvest-source`);
-    return response.status();
-};
+exports.publisher = async page => await getStatus(page, 'publisher/test-publisher') ;
+exports.harvest = async page => await getStatus(page, 'harvest/mock-harvest-source') ;
+exports.datasetStandard = async page => await getStatus(page, 'dataset/example-dataset-number-one') ;
