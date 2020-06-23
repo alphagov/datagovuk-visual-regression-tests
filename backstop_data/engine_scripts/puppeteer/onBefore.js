3f151f953d7e65251ddb14d89512dc37191ad871
owenatgov <64783893+owenatgov@users.noreply.github.com>
2020-06-23 15:14:54 +0100

    At timeout cancel to cookie clear

--- backstop_data/engine_scripts/puppeteer/onBefore.js
+++ backstop_data/engine_scripts/puppeteer/onBefore.js
@@ -1,6 +1,8 @@
 const clearCookies = require('./utils/clear-cookies');
 
 module.exports = (page, scenario, vp) => {
+    page.setDefaultNavigationTimeout(0);
+
     // Set the label string to be used across config scripts
     const label = `${scenario.label}, ${vp.label}`;
     clearCookies(page, scenario, label);
