b6428ac23a1ebc424d776beb5ab42a3b7e9fdbab
owenatgov <owen.jones@digital.cabinet-office.gov.uk>
2020-09-01 10:37:19 +0100

    Add hideSelectors option into individual scenarios

--- backstop_data/engine_scripts/puppeteer/utils/close-admin-toolbar.js
+++
@@ -1,6 +0,0 @@
-// Closes the admin toolbar that pops up on certain views
-
-module.exports = async (page) => {
-    console.log('closing ckan admin toolbar...');
-    await page.click('#flHideToolBarButton');
-}
