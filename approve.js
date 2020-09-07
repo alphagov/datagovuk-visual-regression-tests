d5c763c54041eb873f8ff1628a336526599d84c1
owenatgov <owen.jones@digital.cabinet-office.gov.uk>
2020-09-07 15:50:59 +0100

    Add env variable override to approve script

--- approve.js
+++ approve.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const backstop = require('backstopjs');
 
 (async () => {
-    const config = process.argv[2] ? process.argv[2] : '2.7';
+    const config = process.argv[2] || process.env.CKAN_VERSION || '2.7';
 
     console.log(`You are about to approve the most recently run tests for data.gov.uk: ${config}. Please remember that the approval process will only run against the most recently run tests, including filters eg: if you just ran a 2.8 homepage test, only those homepage tests will be approved for the 2.8 config list.`);
 
